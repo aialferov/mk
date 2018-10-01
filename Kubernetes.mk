280f498db8c8f99566f288c7b879045660e628cf
Anton Alferov <anton@alferov.me>
2018-10-01 18:05:26 +0200

    Ability to address pod by index on pod related actions
    Also add target to list the pods

--- Kubernetes.mk
+++ Kubernetes.mk
@@ -1,13 +1,17 @@
 POD_NAME = $(shell kubectl get po \
 				-l run=$(PROJECT) \
-				-o jsonpath='{.items[*].metadata.name}')
+				-o jsonpath='{.items[$*].metadata.name}')
 
 KUBE_USAGE_PADDING = 13
 
 kube-run:
 	kubectl run $(PROJECT) -it --image=$(IMAGE) -- $(RUN_ARGS)
 
-kube-join:
+kube-pods:
+	kubectl get pod -l run=$(PROJECT) -o wide
+
+kube-join: kube-join-0
+kube-join-%:
 	kubectl exec \
 		-it $(POD_NAME) -c $(PROJECT) -- \
 		erl \
@@ -16,10 +20,12 @@ kube-join:
 			-sname $(PROJECT)-$$RANDOM \
 			-setcookie $(PROJECT)
 
-kube-shell:
+kube-shell: kube-shell-0
+kube-shell-%:
 	kubectl exec -it $(POD_NAME) -c $(PROJECT) sh
 
-kube-attach:
+kube-attach: kube-attach-0
+kube-attach-%:
 	kubectl attach -it $(POD_NAME) -c $(PROJECT)
 
 kube-upgrade:
@@ -28,11 +34,9 @@ kube-upgrade:
 kube-delete:
 	kubectl delete deployment $(PROJECT)
 
-kube-logs:
-	kubectl logs $(POD_NAME)
-
-kube-logs-f:
-	kubectl logs $(POD_NAME) -f
+kube-logs: kube-logs-0
+kube-logs-%:
+	kubectl logs $(POD_NAME) $(LOGS_ARGS)
 
 kube-help: kube-usage
 kube-usage:
@@ -48,19 +52,20 @@ define usage-kube-targets
 	@printf \
 		'$(shell printf "    %%-$(KUBE_USAGE_PADDING)s %%s\\\n%.0s" {1..8})'\
 	kube-run "Create deployment \"$(PROJECT)\" using image \"$(IMAGE)\""  \
+	kube-pods "Show pods of the deployment \"$(PROJECT)\"" \
 	kube-join "Remsh to Erlang application in running pod \"$(POD_NAME)\"" \
 	kube-shell "Exec shell in running pod \"$(POD_NAME)\"" \
 	kube-attach "Attach to running pod \"$(POD_NAME)\"" \
 	kube-upgrade "Upgrade deployment \"$(PROJECT)\" to image \"$(IMAGE)\"" \
 	kube-logs "Print running \"$(POD_NAME)\" pod logs" \
-	kube-logs-f "Same as \"kube-logs\" but follow log output" \
 	kube-delete "Delete deployment \"$(PROJECT)\""
 endef
 
 define usage-kube-variables
 	@printf \
-		'$(shell printf "    %%-$(KUBE_USAGE_PADDING)s %%s\\\n%.0s" {1..3})' \
+		'$(shell printf "    %%-$(KUBE_USAGE_PADDING)s %%s\\\n%.0s" {1..4})' \
 	PROJECT "Deployment name (current: \"$(PROJECT)\")" \
 	IMAGE "Image to deploy or upgrade deployment (current: \"$(IMAGE)\")" \
-	RUN_ARGS "Container entrypoint arguments (current: \"$(RUN_ARGS)\")"
+	RUN_ARGS "Container entrypoint arguments (current: \"$(RUN_ARGS)\")" \
+	LOG_ARGS "Arguments for the logs target"
 endef
