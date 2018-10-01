d825d9dc3797d2aaf1ec6c7a02774b282f84418e
Anton Alferov <anton@alferov.me>
2018-10-01 21:52:22 +0200

    Update kube-usage info

--- Kubernetes.mk
+++ Kubernetes.mk
@@ -1,14 +1,14 @@
-POD_NAME = $(shell kubectl get po \
+POD_NAME = $(shell kubectl get pods \
 				-l run=$(PROJECT) \
 				-o jsonpath='{.items[$*].metadata.name}')
 
-KUBE_USAGE_PADDING = 13
+KUBE_USAGE_PADDING = 17
 
 kube-run:
 	kubectl run $(PROJECT) -it --image=$(IMAGE) -- $(RUN_ARGS)
 
 kube-pods:
-	kubectl get pod -l run=$(PROJECT) -o wide
+	kubectl get pods -l run=$(PROJECT) -o wide
 
 kube-join: kube-join-0
 kube-join-%:
@@ -51,21 +51,21 @@ kube-usage:
 define usage-kube-targets
 	@printf \
 		'$(shell printf "    %%-$(KUBE_USAGE_PADDING)s %%s\\\n%.0s" {1..8})'\
-	kube-run "Create deployment \"$(PROJECT)\" using image \"$(IMAGE)\""  \
-	kube-pods "Show pods of the deployment \"$(PROJECT)\"" \
-	kube-join "Remsh to Erlang application in running pod \"$(POD_NAME)\"" \
-	kube-shell "Exec shell in running pod \"$(POD_NAME)\"" \
-	kube-attach "Attach to running pod \"$(POD_NAME)\"" \
+	kube-run "Create a deployment \"$(PROJECT)\" using image \"$(IMAGE)\""  \
+	kube-pods "Show pod list of the deployment \"$(PROJECT)\"" \
+	kube-join[-N] "Remsh to the running Erlang application in the Nth pod" \
+	kube-shell[-N] "Exec shell in the Nth pod" \
+	kube-attach[-N] "Attach to the Nth pod" \
 	kube-upgrade "Upgrade deployment \"$(PROJECT)\" to image \"$(IMAGE)\"" \
-	kube-logs "Print running \"$(POD_NAME)\" pod logs" \
+	kube-logs[-N] "Print the Nth pod logs" \
 	kube-delete "Delete deployment \"$(PROJECT)\""
 endef
 
 define usage-kube-variables
 	@printf \
 		'$(shell printf "    %%-$(KUBE_USAGE_PADDING)s %%s\\\n%.0s" {1..4})' \
-	PROJECT "Deployment name (current: \"$(PROJECT)\")" \
-	IMAGE "Image to deploy or upgrade deployment (current: \"$(IMAGE)\")" \
-	RUN_ARGS "Container entrypoint arguments (current: \"$(RUN_ARGS)\")" \
+	PROJECT "Used as deployment name (current: \"$(PROJECT)\")" \
+	IMAGE "Image to create or upgrade deployment (current: \"$(IMAGE)\")" \
+	RUN_ARGS "Container entrypoint run arguments (current: \"$(RUN_ARGS)\")" \
 	LOG_ARGS "Arguments for the logs target"
 endef
